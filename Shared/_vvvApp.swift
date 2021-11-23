f139b9afec0842ae9bfa73ffd82fe56e675866be
belyaevios <belyaev2021@icloud.com>
2021-11-23 14:53:46 +0300

    Initial Commit

---
+++ Shared/_vvvApp.swift
@@ -0,0 +1,17 @@
+//
+//  _vvvApp.swift
+//  Shared
+//
+//  Created by Владимир Беляев on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct _vvvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
